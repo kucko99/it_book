5bd528f7b0705532f62d650acc0022261fcf4885
Samuel Kucko <kucko@elcom.eu>
2024-01-10 14:39:38 +0100

    Change default implementationAdd search book implementation

---
+++ lib/Book.dart
@@ -0,0 +1,19 @@
+class Book {
+  final String title;
+  final String subtitle;
+  final String isbn13;
+  final String price;
+  final String image;
+
+  Book({required this.title, required this.subtitle, required this.isbn13, required this.price, required this.image});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      title: json['title'],
+      subtitle: json['subtitle'],
+      isbn13: json['isbn13'],
+      price: json['price'],
+      image: json['image'],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,123 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'Book.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ITBookApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ITBookApp extends StatelessWidget {
+  const ITBookApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IT Book',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.brown
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SearchBookScreen(title: 'IT Book Search'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class SearchBookScreen extends StatefulWidget {
+  const SearchBookScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SearchBookScreen> createState() => _SearchBookScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SearchBookScreenState extends State<SearchBookScreen> {
+  TextEditingController _searchInputController = TextEditingController();
+  List<Book> _searchedBooks = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.brown, // Farba pre AppBar
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          SearchBookTextInput(controller: _searchInputController),
+          SearchBookButton(
+            onButtonPressed: () {
+              _searchBooks(_searchInputController.text);
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
     );
+  }
+
+  Future<void> _searchBooks(String query) async {
+    final response = await http.get(Uri.parse('https://api.itbook.store/1.0/search/$query'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        _searchedBooks = (data['books'] as List).map((book) => Book.fromJson(book)).toList();
+        printInfoAboutBooksList(_searchedBooks);
+      });
+    } else {
+      throw Exception('Failed to load books.');
+    }
+  }
+
+  void printInfoAboutBooksList(List<Book> result) {
+    print('Number of books: ${result.length}');
+    //print('List of books: $result');
+
+    for (int i = 0; i < _searchedBooks.length; i++) {
+      print('Book ${i+1}:');
+      print('Title: ${_searchedBooks[i].title}');
+      print('Subtitle: ${_searchedBooks[i].subtitle}');
+      print('ISBN-13: ${_searchedBooks[i].isbn13}');
+      print('Price: ${_searchedBooks[i].price}');
+      print('Image: ${_searchedBooks[i].image}');
+      print('\n');
+    }
+  }
+}
+
+class SearchBookTextInput extends StatelessWidget {
+  final TextEditingController controller;
+
+  SearchBookTextInput({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+      child: TextField(
+        controller: controller,
+        decoration: const InputDecoration(
+          labelText: 'Enter name of search book',
+          contentPadding: EdgeInsets.only(top: 5)
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBookButton extends StatelessWidget {
+  final VoidCallback onButtonPressed;
+
+  SearchBookButton({required this.onButtonPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onButtonPressed, child: Text('Search book'));
   }
 }
